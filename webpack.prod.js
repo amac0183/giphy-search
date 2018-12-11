81767a875c9cf7657b7a606c0af6667bbfb95ecd
Andrew Hayakawa <amac0183@gmail.com>
2018-12-10 20:50:43 -0500

    Added webpack prod

---
+++ webpack.prod.js
@@ -0,0 +1,20 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+});
